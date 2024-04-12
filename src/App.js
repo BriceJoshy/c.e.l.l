c8b19e69de8e8207fec90da2a8796eff3a7dd3d9
BRICE JOSHY <bricejoshy99@gmail.com>
2024-04-12 19:29:37 +0530

    loading screen finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import WelcomePage from "./components/WelcomePage";
 
+import LoadingScreen from "./components/LoadingScreen";
+
 const App = () => {
-  return (
-    <>
-      <WelcomePage />
-    </>
-  );
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setisLoading(false);
+      }, 5000);
+    };
+
+    fakeDataFetch();
+  }, []);
+  return isLoading ? <LoadingScreen /> : <WelcomePage />;
 };
 
 export default App;
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,19 @@
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+import "../index.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className="bgWelcome">
+      <div className="centreText">
+        <img
+          src={require("../assets/loading.gif")}
+          // width={150}
+          height={250}
+          alt="loading gif"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
