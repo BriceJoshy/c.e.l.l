85a4a8aa4e189ce04fa9897f5796d176c0143c1f
BRICE JOSHY <bricejoshy99@gmail.com>
2024-04-24 20:52:10 +0530

    Login Page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -8,6 +8,15 @@ const LoginPage = () => {
       <div className="LoginPage">
         <LoginNavBar />
       </div>
+      <div style={{ marginLeft: "100px", color: "gray", marginBottom: "15px" }}>
+        START FOR FREE
+      </div>
+      <div style={{ fontSize: "40px", marginLeft: "100px" }}>
+        Create new account <span style={{ color: "#1d8ff4" }}>.</span>
+      </div>
+      <div style={{ marginLeft: "100px", color: "gray", marginTop: "25px" }}>
+        Already a member? <span style={{ color: "#1d8ff4" }}>Log In</span>
+      </div>
     </div>
   );
 };
