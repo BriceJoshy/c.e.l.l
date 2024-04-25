d77ad2ff4469a865d0767763c8be5a65d9f67c6f
BRICE JOSHY <bricejoshy99@gmail.com>
2024-04-25 15:22:20 +0530

    Email part with trailing icon

--- src/components/LoginNavBar.jsx
+++ src/components/LoginNavBar.jsx
@@ -2,12 +2,8 @@ const LoginNavBar = () => {
   return (
     <div className="LoginNavBar">
       <div className="row">
-        <img
-          src={require("../assets/darkMatter.gif")}
-          height={50}
-          alt="logo image"
-        />
-        Welcome Back.
+        <img src={require("../assets/darkMatter.gif")} height={50} alt="logo" />
+        Welcome to C.E.L.L.
       </div>
       <div className="DimSpace">Home</div>
       <div className="DimSpace">Join</div>
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import LoginNavBar from "./LoginNavBar";
-import Transition from "./Transition";
-
+import Name from "./LoginPageName";
+import { useState } from "react";
 const LoginPage = () => {
+  const [inputEmailValue, setInputEmailValue] = useState("");
+  const handleEmailInputChange = (event) => {
+    setInputEmailValue(event.target.value);
+  };
   return (
     <div className="loginBG">
       <div className="LoginPage">
@@ -14,8 +18,41 @@ const LoginPage = () => {
       <div style={{ fontSize: "40px", marginLeft: "100px" }}>
         Create new account <span style={{ color: "#1d8ff4" }}>.</span>
       </div>
-      <div style={{ marginLeft: "100px", color: "gray", marginTop: "25px" }}>
+      <div
+        style={{
+          marginLeft: "100px",
+          color: "gray",
+          marginTop: "25px",
+          fontSize: "15px",
+        }}
+      >
         Already a member? <span style={{ color: "#1d8ff4" }}>Log In</span>
+      </div>
+      <div style={{ display: "inline-block" }}>
+        <Name />
+      </div>
+      <div>
+        <div className="EmailInput">
+          <div className="row">
+            <input
+              className="EmailTextField"
+              value={inputEmailValue}
+              onChange={handleEmailInputChange}
+              required
+            />
+            <label
+              className={`floating-label ${inputEmailValue ? "active" : ""}`}
+            >
+              Email
+            </label>
+            <img
+              src={require("../assets/email.png")}
+              alt="name"
+              height={"20px"}
+              style={{ marginRight: "20px" }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
