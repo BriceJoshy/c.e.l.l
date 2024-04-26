32d4349047a9b7eb1604c070bef74896fb12dedf
BRICE JOSHY <bricejoshy99@gmail.com>
2024-04-26 17:58:08 +0530

    installed tailwind css

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -3,11 +3,11 @@ import "./style.css";
 
 const HomeScreen = () => {
   return (
-    <>
-      <div className="row">
-        <div className=""></div>
-      </div>
-    </>
+    <aside className="h-screen">
+      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center"></div>
+      </nav>
+    </aside>
   );
 };
 
