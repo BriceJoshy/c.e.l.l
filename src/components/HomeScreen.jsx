6803f767acab9ee751afe0018d35515313e12dc6
BRICE JOSHY <bricejoshy99@gmail.com>
2024-04-28 10:30:42 +0530

    login page design

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,13 +1,9 @@
-import React from "react";
 import "./style.css";
-
 const HomeScreen = () => {
   return (
-    <aside className="h-screen">
-      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-        <div className="p-4 pb-2 flex justify-between items-center"></div>
-      </nav>
-    </aside>
+    <div className="home-screen">
+      <div className="sidebar"></div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/components/HomeScreen.jsx"],
   theme: {
     extend: {},
   },
