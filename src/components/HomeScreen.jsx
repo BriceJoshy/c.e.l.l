4d7b4309dc15db264596b22ba844dfb960bb843e
BRICE JOSHY <bricejoshy99@gmail.com>
2024-04-28 12:10:17 +0530

    user details

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,7 +2,18 @@ import "./style.css";
 const HomeScreen = () => {
   return (
     <div className="home-screen">
-      <div className="sidebar"></div>
+      <div className="container">
+        <div className="sidebar">
+          <div className="head">
+            <div className="user-img">
+              <div className="user-details">
+                <p className="title">Web Developer</p>
+                <p className="title">Web Deloper</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
